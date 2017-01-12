ccce4e7eadcc6fd43b5229fd46c1a0fe69740f8a
Zarek Siegel <zarek.siegel@gmail.com>
2017-01-12 17:09:30 -0500

    set up setup.py

--- setup.py
+++ setup.py
@@ -6,7 +6,17 @@
   # created 2017/01/12
   #
 
-def main():
+from setuptools import setup
 
+setup(
+	name='zdata',
+	version='0.1',
+	description='zarek\'s data container package',
+	url='https://github.com/kerazarek/zdata',
+	author='zarek',
+	author_email='zarek.siegel@gmail.com',
+	license='MIT',
+	packages=['zdata'],
+	zip_safe=False
+)
 
-if __name__ == "__main__": main()
